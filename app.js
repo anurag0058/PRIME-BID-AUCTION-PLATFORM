bab1049975793a4505cea27e6e91f580bcb58dc4
anurag0058 <rathourasr0508@gmail.com>
2025-05-12 16:36:45 +0530

    usercontrollers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { connection } from "./database/connection.js";
+import { errorMiddleware } from "./middlewares/error.js";
 
 const app = express();
 config({
@@ -27,5 +28,7 @@ app.use(fileUpload({
 })
 );
 
-connection()
+connection();
+app.use(errorMiddleware)
+
 export default app;
