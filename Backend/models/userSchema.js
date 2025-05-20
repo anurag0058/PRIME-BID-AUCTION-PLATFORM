f74a5411d8334d0f5f2bb46f16ff5ec454dd5afc
anurag0058 <rathourasr0508@gmail.com>
2025-05-21 00:54:51 +0530

    changes

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -37,8 +37,8 @@ const userSchema = new mongoose.Schema({
         bankAccountName: String,
         bankName: String,
       },
-      easypaisa : {
-        easypaisaAccountNumber: Number
+      Phonepe : {
+        PhonepeAccountNumber: Number
       },
       paypal: {
         paypalEmail: String,
