49f4141d2ab94f3bf662b77b77ae5e77344dc547
anurag0058 <rathourasr0508@gmail.com>
2025-05-21 00:16:17 +0530

    login

--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -26,21 +26,21 @@ const userSlice = createSlice({
       state.isAuthenticated = false;
       state.user = {};
     },
-    // loginRequest(state, action) {
-    //   state.loading = true;
-    //   state.isAuthenticated = false;
-    //   state.user = {};
-    // },
-    // loginSuccess(state, action) {
-    //   state.loading = false;
-    //   state.isAuthenticated = true;
-    //   state.user = action.payload.user;
-    // },
-    // loginFailed(state, action) {
-    //   state.loading = false;
-    //   state.isAuthenticated = false;
-    //   state.user = {};
-    // },
+    loginRequest(state, action) {
+      state.loading = true;
+      state.isAuthenticated = false;
+      state.user = {};
+    },
+    loginSuccess(state, action) {
+      state.loading = false;
+      state.isAuthenticated = true;
+      state.user = action.payload.user;
+    },
+    loginFailed(state, action) {
+      state.loading = false;
+      state.isAuthenticated = false;
+      state.user = {};
+    },
     // fetchUserRequest(state, action) {
     //   state.loading = true;
     //   state.isAuthenticated = false;
@@ -108,26 +108,26 @@ export const register = (data) => async (dispatch) => {
   }
 };
 
-// export const login = (data) => async (dispatch) => {
-//   dispatch(userSlice.actions.loginRequest());
-//   try {
-//     const response = await axios.post(
-//       "http://localhost:5000/api/v1/user/login",
-//       data,
-//       {
-//         withCredentials: true,
-//         headers: { "Content-Type": "application/json" },
-//       }
-//     );
-//     dispatch(userSlice.actions.loginSuccess(response.data));
-//     toast.success(response.data.message);
-//     dispatch(userSlice.actions.clearAllErrors());
-//   } catch (error) {
-//     dispatch(userSlice.actions.loginFailed());
-//     toast.error(error.response.data.message);
-//     dispatch(userSlice.actions.clearAllErrors());
-//   }
-// };
+export const login = (data) => async (dispatch) => {
+  dispatch(userSlice.actions.loginRequest());
+  try {
+    const response = await axios.post(
+      "http://localhost:5000/api/v1/user/login",
+      data,
+      {
+        withCredentials: true,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    dispatch(userSlice.actions.loginSuccess(response.data));
+    toast.success(response.data.message);
+    dispatch(userSlice.actions.clearAllErrors());
+  } catch (error) {
+    dispatch(userSlice.actions.loginFailed());
+    toast.error(error.response.data.message);
+    dispatch(userSlice.actions.clearAllErrors());
+  }
+};
 
 export const logout = () => async (dispatch) => {
   try {
